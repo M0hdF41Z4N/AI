6cb7d0f8bc104880977b401394a11f6a3d043ba7
Mohd Faizan <31098815+M0hdF41Z4N@users.noreply.github.com>
2025-03-06 05:36:05 +0000

    Added Google OAuth Component

---
+++ src/constants/urls.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://life-with-ai.onrender.com';
+export const LOGIN_URL = `${BASE_URL}/api/auth/google`;
+export const GOOGLE_MAIL_SCOPE_URL = 'https://mail.google.com/'
